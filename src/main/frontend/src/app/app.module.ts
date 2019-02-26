c3f6229ab341fae053393761e46b934d53033acb
AtomCats <toni-e@yandex.ru>
2019-02-26 22:25:49 +0300

    Test works

--- src/main/frontend/src/app/app.module.ts
+++ src/main/frontend/src/app/app.module.ts
@@ -1,14 +1,23 @@
-import {BrowserModule} from '@angular/platform-browser';
+
+import {HttpClientModule} from '@angular/common/http';
 import {NgModule} from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http';
-
-import {AppComponent} from './app.component';
-import {EcommerceComponent} from './ecommerce/ecommerce.component';
-import {ProductsComponent} from './ecommerce/products/products.component';
-import {ShoppingCartComponent} from './ecommerce/shopping-cart/shopping-cart.component';
-import {OrdersComponent} from './ecommerce/orders/orders.component';
+import {MatNativeDateModule} from '@angular/material';
+import {BrowserModule} from '@angular/platform-browser';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {DemoMaterialModule} from './material-module';
+import {AppComponent} from "./app.component";
 import {EcommerceService} from "./ecommerce/services/EcommerceService";
+import {EcommerceComponent} from "./ecommerce/ecommerce.component";
+import {ProductsComponent} from "./ecommerce/products/products.component";
+import {OrdersComponent} from "./ecommerce/orders/orders.component";
+import {ShoppingCartComponent} from "./ecommerce/shopping-cart/shopping-cart.component";
+import { registerLocaleData } from '@angular/common';
+import localeRu from '@angular/common/locales/ru';
+
+registerLocaleData(localeRu)
+
 
 @NgModule({
   declarations: [
@@ -20,12 +29,20 @@ import {EcommerceService} from "./ecommerce/services/EcommerceService";
   ],
   imports: [
     BrowserModule,
-    HttpClientModule,
+    BrowserAnimationsModule,
     FormsModule,
-    ReactiveFormsModule
+    HttpClientModule,
+    DemoMaterialModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
   ],
+  entryComponents: [AppComponent],
   providers: [EcommerceService],
   bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule {}
+
+
+/**  Copyright 2018 Google Inc. All Rights Reserved.
+ Use of this source code is governed by an MIT-style license that
+ can be found in the LICENSE file at http://angular.io/license */
--- src/main/frontend/src/app/ecommerce/ecommerce.component.ts
+++ src/main/frontend/src/app/ecommerce/ecommerce.component.ts
@@ -1,44 +1,37 @@
-import {Component, OnInit, ViewChild} from "@angular/core";
 import {ProductsComponent} from "./products/products.component";
 import {ShoppingCartComponent} from "./shopping-cart/shopping-cart.component";
 import {OrdersComponent} from "./orders/orders.component";
+import {MediaMatcher} from '@angular/cdk/layout';
+import {ChangeDetectorRef, Component, OnDestroy} from '@angular/core';
 
 @Component({
   selector: 'app-ecommerce',
   templateUrl: './ecommerce.component.html',
   styleUrls: ['./ecommerce.component.css']
 })
-export class EcommerceComponent implements OnInit {
-  private collapsed = true;
-  orderFinished = false;
+export class EcommerceComponent implements OnDestroy {
+  mobileQuery: MediaQueryList;
 
-  @ViewChild('productsC')
-  productsC: ProductsComponent;
+  fillerNav = Array.from({length: 50}, (_, i) => `Nav Item ${i + 1}`);
 
-  @ViewChild('shoppingCartC')
-  shoppingCartC: ShoppingCartComponent;
+  fillerContent = Array.from({length: 50}, () =>
+    `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+       labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+       laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+       voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+       cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`);
 
-  @ViewChild('ordersC')
-  ordersC: OrdersComponent;
+  private _mobileQueryListener: () => void;
 
-  constructor() {
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
   }
 
-  ngOnInit() {
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
   }
 
-  toggleCollapsed(): void {
-    this.collapsed = !this.collapsed;
-  }
-
-  finishOrder(orderFinished: boolean) {
-    this.orderFinished = orderFinished;
-  }
-
-  reset() {
-    this.orderFinished = false;
-    this.productsC.reset();
-    this.shoppingCartC.reset();
-    this.ordersC.paid = false;
-  }
+  shouldRun = [/(^|\.)plnkr\.co$/, /(^|\.)stackblitz\.io$/].some(h => h.test(window.location.host));
 }
